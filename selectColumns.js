d8b61ae58097c58b23efebfc75ab1b6af92d531d
Xuan Bui <xtnbui@mit.edu>
2016-04-05 22:31:49 -0400

    Files ready for bootstrap

---
+++ selectColumns.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	/*var columns = ["Erasers", "Notebooks", "Pencils", "Tapes"];
+	var newRow = $("<tr></tr>");
+
+	for (var i =0; i<columns.size; i++) {
+		var newCol = $("<tr></tr>").text(columns[i]);
+		newRow.append(newCol);
+	}
+
+	$("#categories").append(newRow);*/
+	$(".datepicker").datepicker();
+});
