b90026516875ab690f4d9ac2ac30775b4af66078
Lena Abdalla <labdalla@mit.edu>
2016-04-10 23:23:40 -0400

    some of the views of the update inventory page and brand/category select screen

--- labelSelect.js
+++ labelSelect.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
 
+  //click handler to show tab
+  $("#brand a").click(function(event) {
+    $(this).tab('show')
+  });
+
+  //click handler to show tab
+  $("#category a").click(function(event) {
+    $(this).tab('show')
+  });
+
+  $("#pencil-icon").click(function(event) {
+    window.location.href = "./updateInventory.html";
+  });
+
+
 });
--- updateInventory.js
+++ updateInventory.js
@@ -1,3 +1,4 @@
 $(document).ready(function() {
-
+  // from http://stackoverflow.com/questions/1304378/jquery-web-page-height/1304384#1304384
+  $("#items-container").css("height", 0.6*$(document).height()); //set the height of the items container to be 3/4 of the document's height
 });
