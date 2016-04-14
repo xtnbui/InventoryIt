66c5ce9cd314bc40e79b73fc11cc727efc428ac6
Xuan Bui <xtnbui@mit.edu>
2016-04-14 02:28:04 -0400

    Fixing tabs

--- labelSelect.js
+++ labelSelect.js
@@ -1,15 +1,5 @@
 $(document).ready(function() {
 
-  //click handler to show tab
-  $("#brand a").click(function(event) {
-    $(this).tab('show');
-  });
-
-  //click handler to show tab
-  $("#category a").click(function(event) {
-    $(this).tab('show');
-  });
-
   //clicking on pencils icon or title link redirects you to update inv page
   $("#pencil-icon").click(function(event) {
     window.location.href = "./updateInventory.html";
