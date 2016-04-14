e677521596190fb5d5bcbe58c192edbf47c1eb54
Lena Abdalla <labdalla@mit.edu>
2016-04-13 22:01:42 -0400

    made icon button titles links - pencils link redirects to update inv page

--- labelSelect.js
+++ labelSelect.js
@@ -10,8 +10,12 @@ $(document).ready(function() {
     $(this).tab('show');
   });
 
-
+  //clicking on pencils icon or title link redirects you to update inv page
   $("#pencil-icon").click(function(event) {
+    window.location.href = "./updateInventory.html";
+  });
+
+  $("#pencils").click(function(event) {
     window.location.href = "./updateInventory.html";
   });
 
