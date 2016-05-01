a2cbf37ca8edfbbcabfc3ecd290d3d64c8e5603e
megvuyyuru <megvuyyuru@gmailcom>
2016-05-01 04:27:47 -0400

    Data now pulled from Firebase and table generated via JS

--- updateInventory.js
+++ updateInventory.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  var myDataRef = new Firebase('https://blinding-inferno-865.firebaseio.com/');
+  var ref = new Firebase('https://blinding-inferno-865.firebaseio.com/Name');
 
   // from http://stackoverflow.com/questions/1304378/jquery-web-page-height/1304384#1304384
   $("#items-container").css("height", 0.6*$(document).height()); //set the height of the items container to be 3/4 of the document's height
@@ -8,6 +8,84 @@ $(document).ready(function() {
   var itemtoQuantityRecord = {};
   var itemtoCurrentIndex = {};
 
+  var table = document.getElementById("update_table");
+
+  ref.on("value", function(snapshot) {
+    var table = document.getElementById("update_table");
+    snapshot.forEach(function(data) {
+
+      obj_name = data.key();
+      obj_attr = data.val();
+
+      var tr = document.createElement('TR');
+      var td = document.createElement('TD');
+        td.width='10%';
+        img = document.createElement('IMG');
+        img.setAttribute("class", "thumbnail-image img-responsive");
+        img.setAttribute("src", obj_attr["Image"]);
+        td.appendChild(img);
+      tr.appendChild(td);
+
+      var td_2 = document.createElement('TD');
+        td_2.width='70%';
+        var p = document.createElement("p");
+        p.setAttribute("class", "pencil-name top no-padding");
+        p.appendChild(document.createTextNode(obj_name));
+        td_2.appendChild(p);
+
+          var bar_div = document.createElement('DIV');
+          bar_div.setAttribute("class", "progress bottom progress-size");
+            var bar_div2 = document.createElement('DIV');
+            bar_div2.setAttribute("class", "progress-bar progress-color");
+            bar_div2.setAttribute("role", "progressbar");
+            bar_div2.setAttribute("style", "width:10%");
+            bar_div2.setAttribute("aria-valuenow", obj_attr["In Stock"]);
+            bar_div2.setAttribute("aria-valuemax", obj_attr["Max Quantity"]);
+            bar_div2.setAttribute("aria-valuemin", "0");
+              // span = document.createElement('SPAN');
+              // span.setAttribute("class", "sr-only");
+              // span.innerHTML = amt + "% Complete";
+              // bar_div2.appendChild(span);
+            bar_div.appendChild(bar_div2);
+          td_2.appendChild(bar_div);
+      tr.appendChild(td_2)
+
+        var td_3 = document.createElement('TD');
+          td_3.width='10%';
+          textarea = document.createElement('TEXTAREA')
+          textarea.appendChild(document.createTextNode(obj_attr["In Stock"]));
+          textarea.setAttribute("wrap", "off");
+          textarea.setAttribute("class", "form-control inputted_quantity");
+          textarea.setAttribute("id", "inputted_quantity-3");
+          td_3.appendChild(textarea);
+      tr.appendChild(td_3);
+
+      var td_4 = document.createElement('TD');
+        td_4.width='10%'
+          button = document.createElement('BUTTON')
+          button.innerHTML = 'Save'
+          button.setAttribute("id", "save-btn3");
+          button.setAttribute("type", "button");
+          button.setAttribute("class", "btn btn-primary btn-sm btn-text center-block save-btn");
+        td_4.appendChild(button);
+      tr.appendChild(td_4);
+
+      var td_5 = document.createElement('TD');
+        td_5.width='10%'
+          button2 = document.createElement('BUTTON')
+          button2.innerHTML = 'Undo'
+          button2.setAttribute("id", "save-btn3");
+          button2.setAttribute("type", "button");
+          button2.setAttribute("class", "btn btn-primary btn-sm btn-text center-block save-btn");
+        td_5.appendChild(button2);
+      tr.appendChild(td_5);        
+
+
+      table.appendChild(tr);
+
+    });
+  });
+
   //initialize all the currentIndices
   var numItems = 6;
   for (i=0; i<numItems; i++) {
@@ -15,7 +93,7 @@ $(document).ready(function() {
   }
 
 
-  $(".save-btn").click(function(event) {
+  save_button = $(".save-btn").click(function(event) {
     var itemNumber = $(this).attr("data-item-number");
     if (!(itemtoQuantityRecord[itemNumber])) {
       itemtoQuantityRecord[itemNumber] = []; //initialize an array to keep track of the quantities of that item
